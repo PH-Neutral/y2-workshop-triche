52f3c6f86d7052da0e97b707243e53887a89be20
PH_Neutral <pierrehoel.charny@gmail.com>
2021-12-03 15:50:07 +0100

    opponent follow path and turn

--- Assets/Scripts/Game/Opponent.cs
+++ Assets/Scripts/Game/Opponent.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Opponent : MonoBehaviour
 {
+    [SerializeField] FollowPath path;
+    [SerializeField] bool rollBaby = false;
+    [SerializeField] float speed;
     [SerializeField] float pushTime = 1f;
     [SerializeField] float pushSpeed = 10f;
+    int index = 0;
 
     bool left;
 
+    private void Update() {
+        if(rollBaby) {
+            float deltaDistance = speed * Time.deltaTime;
+            Vector3 targetPos = path.GetWaypoint(index).position;
+            Vector3 targetDir = (targetPos - transform.position).normalized;
+            float distanceToTarget = Vector3.Distance(transform.position, targetPos);
+            if(distanceToTarget != 0) {
+                transform.LookAt(targetPos, Vector3.up);
+            }
+            if(deltaDistance >= distanceToTarget) {
+                Move(targetDir * distanceToTarget);
+                index = path.GetNextIndex(index);
+            } else {
+                Move(targetDir * deltaDistance);
+            }
+        }
+    }
 
-
-
-
-
-
+    void Move(Vector3 motion) {
+        transform.position += motion;
+    }
 
     public void CallPush(bool left)
     {
